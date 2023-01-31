ea79e6b0a3d1ecc08f9e0b727a65419147c64212
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-31 16:50:59 +0100

    add private

--- laravel-model-controller/resources/views/pages/home.blade.php
+++ laravel-model-controller/resources/views/pages/home.blade.php
@@ -11,11 +11,11 @@
             @foreach ($movie as $item)
                 <li>
                     <div class="card">
-                        <h6>Title: {{$item['title']}}</h6>
-                        <span>Original Title: {{$item['original_title']}}</span>
-                        <span>Lang: {{$item['nationality']}}</span>
-                        <span>Date: {{$item['date']}}</span>
-                        <span>Vote: {{$item['vote']}}</span>
+                        <h6>Title: {{$item -> title}}</h6>
+                        <span>Original Title: {{$item -> original_title}}</span>
+                        <span>Lang: {{$item -> nationality}}</span>
+                        <span>Date: {{$item -> date}}</span>
+                        <span>Vote: {{$item -> vote}}</span>
                     </div>
                 </li>
             @endforeach
